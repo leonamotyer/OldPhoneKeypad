21913fba83d365b5f1f2c8cb2bef8d002241b3af
L. Motyer <158305618+leonamotyer@users.noreply.github.com>
2024-07-25 18:51:04 -0600

    Add files via upload

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -45,6 +45,65 @@ namespace TestOldPhone
             string expected = "CAB";
             string actual = OldPhonePadTranslator.OldPhonePad("222 2 22#");
             Assert.AreEqual(expected, actual);
-        } 
+        }
+        [Test]
+        public void Test6()
+        {
+            string expected = "LEONA";
+            string actual = OldPhonePadTranslator.OldPhonePad("55533666 662#");
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Test7()
+        {
+            string expected = "LILY";
+            string actual = OldPhonePadTranslator.OldPhonePad("555444555999#");
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Test8()
+        {
+            string expected = "ARYA";
+            string actual = OldPhonePadTranslator.OldPhonePad("27779992#");
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Test9()
+        {
+            string expected = "IRON";
+            string actual = OldPhonePadTranslator.OldPhonePad("444777666 66#");
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Test10()
+        {
+            string expected = "AIO";
+            string actual = OldPhonePadTranslator.OldPhonePad("2444666#");
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Test11()
+        {
+            string expected = "THANK YOU FOR YOUR TIME";
+            string actual = OldPhonePadTranslator.OldPhonePad("84426655099966688033366677709996668877708444633#");
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Test12()
+        {
+            string expected = "DID YOU HEAR ABOUT THE GUY THAT GOT INJURED IN THE PEEKABOO ACCIDENT";
+            string actual = OldPhonePadTranslator.OldPhonePad("3444309996668804433277702 2266688 8084433048899908442804666804446658877733 30444660844330733 33552 22666 66602 222 2224443 33668#");
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Test13()
+        {
+            string expected = "INTERVIEW FOR THE ANSWER";
+            string actual = OldPhonePadTranslator.OldPhonePad("44466833777888444339033366677708443302667777933777#");
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
     } 
 }
